1318f55c403adea39f13c4956ace98505443d220
keltzeleo <65336730+keltzeleo@users.noreply.github.com>
2023-07-13 00:29:31 +0800

    no use

---
+++ .history/src/pages/LibraryMaintanence/TariffChargesMaintanence_20230711003757 2.tsx
@@ -0,0 +1,104 @@
+import { DownOutlined } from '@ant-design/icons';
+import ProTable, { ProColumns } from '@ant-design/pro-table';
+import { Badge, Dropdown, Menu } from 'antd';
+
+const menu = (
+  <Menu>
+    <Menu.Item>Action 1</Menu.Item>
+    <Menu.Item>Action 2</Menu.Item>
+  </Menu>
+);
+
+interface DataType {
+  key: number;
+  date: string;
+  name: string;
+  upgradeNum: string;
+}
+
+const expandedRowRender = () => {
+  const columns: ProColumns<DataType>[] = [
+    { title: 'Date', dataIndex: 'date' },
+    { title: 'Name', dataIndex: 'name' },
+    {
+      title: 'Status',
+      render: () => (
+        <span>
+          <Badge status="success" />
+          Finished
+        </span>
+      ),
+    },
+    { title: 'Upgrade Status', dataIndex: 'upgradeNum' },
+    {
+      title: 'Action',
+      render: () => (
+        <span className="table-operation">
+          <a>Pause</a>
+          <a>Stop</a>
+          <Dropdown overlay={menu}>
+            <a>
+              More <DownOutlined />
+            </a>
+          </Dropdown>
+        </span>
+      ),
+    },
+  ];
+
+  const data: DataType[] = [];
+  for (let i = 0; i < 3; ++i) {
+    data.push({
+      key: i,
+      date: '2014-12-24 23:12:00',
+      name: 'This is production name',
+      upgradeNum: 'Upgraded: 56',
+    });
+  }
+
+  return (
+    <ProTable<DataType>
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+    />
+  );
+};
+
+const columns: ProColumns<DataType>[] = [
+  { title: 'Name', dataIndex: 'name' },
+  { title: 'Platform', dataIndex: 'platform' },
+  { title: 'Version', dataIndex: 'version' },
+  { title: 'Upgraded', dataIndex: 'upgradeNum' },
+  { title: 'Creator', dataIndex: 'creator' },
+  { title: 'Date', dataIndex: 'createdAt' },
+  { title: 'Action', render: () => <a>Publish</a> },
+];
+
+const data: DataType[] = [];
+for (let i = 0; i < 3; ++i) {
+  data.push({
+    key: i,
+    name: 'Screem',
+    platform: 'iOS',
+    version: '10.3.4.5654',
+    upgradeNum: '500',
+    creator: 'Jack',
+    createdAt: '2014-12-24 23:12:00',
+  });
+}
+
+function TariffChargesMaintenance() {
+  return (
+    <ProTable<DataType>
+      scroll={{ x: 1300 }}
+      className="components-table-demo-nested"
+      columns={columns}
+      expandedRowRender={expandedRowRender}
+      dataSource={data}
+      rowKey="key"
+    />
+  );
+}
+
+export default TariffChargesMaintenance;
---
+++ .history/src/pages/ThemeTest/ThemeApp_20230711012252 2.tsx
@@ -0,0 +1,41 @@
+import { ConfigProvider, Switch } from 'antd';
+import React, { useEffect, useState } from 'react';
+// import AppointmentUpdates from "../AppointmentUpdates/AppointmentUpdates";
+import TariffChargesMaintenance from '../LibraryMaintanance/TariffChargesMaintanance';
+// import ThemeTest from "../ThemeTest/ThemeTest";
+
+import dark from '../../tokens/dark.json';
+import light from '../../tokens/light.json';
+
+interface Theme {
+  [key: string]: string;
+}
+
+const ThemeApp: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [token, setToken] = useState<Theme>(light);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = token.colorBgBase;
+  }, [token]);
+
+  const handleThemeChange = () => {
+    setIsDarkMode(!isDarkMode);
+    setToken(isDarkMode ? { ...light } : { ...dark });
+  };
+
+  return (
+    <ConfigProvider theme={{ token }}>
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: 20 }}>
+        <span style={{ marginRight: 10 }}>Light</span>
+        <Switch checked={isDarkMode} onChange={handleThemeChange} />
+        <span style={{ marginLeft: 10 }}>Dark</span>
+      </div>
+      {/* <AppointmentUpdates theme={isDarkMode ? dark : light} /> */}
+      <TariffChargesMaintenance theme={isDarkMode ? dark : light} data={[]} />
+      {/* <ThemeTest /> */}
+    </ConfigProvider>
+  );
+};
+
+export default ThemeApp;
