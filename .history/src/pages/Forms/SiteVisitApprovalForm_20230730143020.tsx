96628e187447f1feb33dba28d09f0fb8c824a7fe
keltzeleo <65336730+keltzeleo@users.noreply.github.com>
2023-07-30 17:10:40 +0800

    form alignment need tom work on

---
+++ .history/src/pages/Forms/SiteVisitApprovalForm_20230730143020.tsx
@@ -0,0 +1,531 @@
+import { Button, Form, Input, Modal, Upload } from "antd";
+import { RcFile } from "antd/es/upload";
+import { UploadChangeParam, UploadFile } from "antd/lib/upload/interface";
+import React, { useState } from "react";
+import IWillFollowYou from "../../customComponents/IWillFollowYou/IWillFollowYou";
+import { acceptedFileTypes } from "../../customConstants/dragDropFileTypes";
+import "./forms.css";
+
+const { Dragger } = Upload;
+
+interface FileListProps extends UploadFile<any> {
+  className?: string;
+  size?: number;
+  uploadDate?: Date;
+}
+
+interface Theme {
+  [key: string]: string;
+}
+
+export interface SiteVisitApprovalFormProps {
+  theme: Theme;
+  onSubmit: (values: any) => void; // Form submission handler prop
+}
+
+const SiteVisitApprovalForm: React.FC<SiteVisitApprovalFormProps> = ({
+  theme,
+}) => {
+  const [form] = Form.useForm();
+
+  const handleSubmit = (values: any) => {
+    // Handle form submission logic here
+    console.log("Form values:", values);
+  };
+  // const { colorBgBase } = theme;
+
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+  const [inputIcValue, setInputIcValue] = useState("");
+
+  const [fileList, setFileList] = useState<FileListProps[]>([]);
+  const [selectedIdType, setSelectedIdType] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isErrorMessageVisible, setIsErrorMessageVisible] = useState(false);
+
+  const handleCancel = () => setPreviewOpen(false);
+
+  const handlePreview = async (file: FileListProps) => {
+    if (file.status === "error") {
+      return;
+    }
+
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj as RcFile);
+    }
+
+    setPreviewImage(file.url || (file.preview as string));
+    setPreviewOpen(true);
+    setPreviewTitle(
+      file.name ||
+        (file.url ? file.url.substring(file.url.lastIndexOf("/") + 1) : "")
+    );
+
+    // Set the file information for display in the modal
+    setFileInfo({
+      name: file.name,
+      size: file.size,
+      uploadDate: file.uploadDate,
+    });
+  };
+
+  const [fileInfo, setFileInfo] = useState<{
+    name?: string;
+    size?: number;
+    uploadDate?: Date;
+  }>({});
+
+  const formatBytes = (bytes: number) => {
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    if (bytes === 0) return "0 Byte";
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString());
+    return Math.round(bytes / Math.pow(1024, i)) + " " + sizes[i];
+  };
+
+  // Placeholder implementation for getBase64 (replace with actual logic)
+  const getBase64 = async (file: RcFile): Promise<string> => {
+    return new Promise<string>((resolve) => {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => resolve(reader.result as string));
+      reader.readAsDataURL(file);
+    });
+  };
+
+  // Placeholder implementation for getChecksum (replace with actual logic)
+  const getChecksum = async (file: RcFile): Promise<number> => {
+    // Replace this with your actual checksum generation logic
+    const sum = file.name
+      .split("")
+      .reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    return sum;
+  };
+
+  const handleError = (errorMsg: string) => {
+    setErrorMessage(errorMsg);
+    setIsErrorMessageVisible(true);
+    setTimeout(() => {
+      setIsErrorMessageVisible(false);
+    }, 5000);
+  };
+
+  const handleChange = async (info: UploadChangeParam<FileListProps>) => {
+    const newFileList = [...info.fileList];
+
+    if (newFileList.length > 5) {
+      newFileList.splice(5);
+    }
+
+    newFileList.forEach((file) => {
+      if (file.status === "error" && !file.url && !file.preview) {
+        file.preview = "placeholder.png";
+      }
+    });
+
+    const duplicateFiles: FileListProps[] = [];
+    const checksumMap: Map<number, FileListProps> = new Map();
+
+    for (const file of newFileList) {
+      const checksum = await getChecksum(file.originFileObj as RcFile);
+      if (checksumMap.has(checksum)) {
+        const duplicateFile = checksumMap.get(checksum);
+        if (duplicateFile && !duplicateFiles.includes(duplicateFile)) {
+          duplicateFiles.push(duplicateFile);
+        }
+        duplicateFiles.push(file);
+      } else {
+        checksumMap.set(checksum, file);
+      }
+    }
+
+    if (duplicateFiles.length > 0) {
+      handleError(
+        `${duplicateFiles[0].name} already exists. For security reasons, please delete the file manually and reupload a new version.`
+      );
+      return;
+    }
+
+    // Calculate the file size and set the size property
+    const calculateFileSize = (file: File): number => {
+      return file.size;
+    };
+
+    // Set the uploadDate property to the current date
+    const getCurrentDate = (): Date => {
+      return new Date();
+    };
+
+    setFileList(
+      newFileList.map((file) => {
+        if (file.status === "error") {
+          return {
+            ...file,
+            thumbUrl: "../../icons/icon_error_sm.png",
+            className: "ant-upload-list-item-error",
+          };
+        }
+        // Calculate the file size and set the size property
+        const fileSize = file.originFileObj
+          ? calculateFileSize(file.originFileObj)
+          : 0;
+        // Set the uploadDate property to the current date
+        const uploadDate = getCurrentDate();
+
+        return {
+          ...file,
+          size: fileSize,
+          uploadDate,
+        };
+      })
+    );
+  };
+
+  const handleRemove = (file: FileListProps) => {
+    setFileList((prevFileList) =>
+      prevFileList.filter((item) => item.uid !== file.uid)
+    );
+  };
+
+  const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+
+    const unsupportedFiles: File[] = [];
+    const validFiles: RcFile[] = [];
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (acceptedFileTypes.includes(file.type)) {
+        validFiles.push(file as RcFile);
+      } else {
+        unsupportedFiles.push(file);
+      }
+    }
+
+    if (unsupportedFiles.length > 0) {
+      handleError("Unsupported file type. Please upload a valid file.");
+      return;
+    }
+
+    const newFileList: FileListProps[] = [];
+    const duplicateFiles: FileListProps[] = [];
+    const checksumMap: Map<number, FileListProps> = new Map();
+
+    for (const file of validFiles) {
+      const checksum = await getChecksum(file);
+      if (checksumMap.has(checksum)) {
+        const duplicateFile = checksumMap.get(checksum);
+        if (duplicateFile && !duplicateFiles.includes(duplicateFile)) {
+          duplicateFiles.push(duplicateFile);
+        }
+        duplicateFiles.push(file as FileListProps);
+      } else {
+        checksumMap.set(checksum, file as FileListProps);
+        const uploadFile: FileListProps = {
+          uid: file.name,
+          name: file.name,
+          status: "done",
+          size: file.size,
+          uploadDate: new Date(), // Set the uploadDate property
+
+          type: file.type,
+          originFileObj: file as RcFile,
+        };
+        newFileList.push(uploadFile);
+      }
+    }
+
+    if (duplicateFiles.length > 0) {
+      handleError(
+        `${duplicateFiles[0].name} already exists. For security reasons, please delete the file manually and reupload a new version.`
+      );
+      return;
+    }
+
+    const existingFileNames = fileList.map((file) => file.name);
+    const redundantFiles = validFiles.filter((file) =>
+      existingFileNames.includes(file.name)
+    );
+
+    if (redundantFiles.length > 0) {
+      handleError(
+        `${redundantFiles[0].name} is redundant. Please remove the duplicate file.`
+      );
+      return;
+    }
+
+    const updatedFileList = [...fileList, ...newFileList];
+
+    if (updatedFileList.length > 5) {
+      updatedFileList.splice(5);
+    }
+
+    setFileList(updatedFileList);
+  };
+
+  // Define the maximum allowed files as a constant variable
+  const MAX_FILES = 5;
+
+  const fileCounter = (
+    <div style={{ marginTop: 8 }}>
+      <p>
+        {fileList.length}{" "}
+        {fileList.length < MAX_FILES
+          ? `out of ${MAX_FILES} files uploaded.`
+          : "Files Limit Reached. Please review & confirm the file lists below."}
+      </p>
+    </div>
+  );
+
+  const fileCounterSquare = (
+    <div style={{ marginTop: 8 }}>
+      <p>
+        {fileList.length}{" "}
+        {fileList.length < MAX_FILES
+          ? `out of ${MAX_FILES} files uploaded.`
+          : "files finished uploading. Please review & confirm the file lists below."}
+      </p>
+    </div>
+  );
+
+  const isUploadDisabled = fileList.length >= 5;
+
+  // Refactor the upload button components to a single component
+  const UploadButton: React.FC<{ isSquare: boolean }> = ({ isSquare }) => (
+    <div
+      style={{
+        width: "auto",
+        height: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "0 16",
+          marginTop: isSquare ? -48 : -36,
+        }}
+      >
+        <p className="ant-upload-drag-icon">
+          <img src="../icons/icon_upload.png" alt="Drag and Drop Icon" />
+        </p>
+
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+
+        <p className="ant-upload-hint" style={{ padding: 16 }}>
+          Support individual and bulk file uploads, please submit the required
+          files as needed. {<br />}{" "}
+          <span style={{ fontSize: 9, fontWeight: 600 }}>
+            ( only PNG, JPG & PDF files are allowed. )
+          </span>
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          bottom: "16",
+          left: "50%",
+          fontSize: 12,
+          transform: "translate(-50%, 0%)",
+          height: "auto",
+          width: "80%",
+          padding: 8,
+          borderRadius: 8,
+          border: "1px dashed #00a991",
+          opacity: isUploadDisabled ? 0.5 : 1,
+          pointerEvents: isUploadDisabled ? "none" : "auto",
+          marginTop: isSquare ? -10 : -30,
+        }}
+      >
+        {isSquare ? fileCounterSquare : fileCounter}
+      </div>
+    </div>
+  );
+
+  // Remove duplicate code and use the UploadButton component
+  const uploadButton = <UploadButton isSquare={false} />;
+  const uploadButtonSquare = <UploadButton isSquare={true} />;
+
+  const getListItemClassName = (file: FileListProps): string => {
+    return file.status === "done" ? "ant-upload-list-item-done" : "";
+  };
+
+  return (
+    <Form form={form} onFinish={handleSubmit}>
+      <div
+        className="drag-drop-container"
+        style={{
+          height: "100vh",
+          backgroundColor: "theme.colorBgBase",
+        }}
+      >
+        <div className="top-section">
+          <div
+            style={{
+              color: "#f3f6f9",
+              borderRadius: 16,
+              padding: "0 0 0 16",
+              top: 0,
+              justifyContent: "center",
+              backgroundColor: "#00a991",
+              fontSize: 12,
+            }}
+          >
+            <h2>Site Visit Approval Form</h2>
+          </div>
+        </div>
+        <div className="top-section" style={{ width: "100vh" }}></div>
+        <div className="content-container" style={{ display: "flex" }}>
+          <div
+            className="left-section"
+            style={{ display: "flex", flexDirection: "column" }}
+          >
+            <div
+              className="upload-area"
+              style={{
+                width: "250px",
+                display: "inline-block",
+                flexDirection: "column",
+                height: "450px",
+              }}
+              onDrop={handleDrop}
+              onDragOver={(e) => e.preventDefault()}
+            >
+              <Dragger
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                fileList={fileList}
+                onPreview={handlePreview}
+                onChange={handleChange}
+                onRemove={handleRemove}
+                listType="picture"
+                showUploadList={{ showRemoveIcon: true }}
+                accept=".pdf,.png, .jpg"
+                style={{ marginRight: 8 }}
+                multiple
+              >
+                <div
+                  style={{
+                    display: "block",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                  }}
+                >
+                  {uploadButton}
+                </div>
+              </Dragger>
+            </div>
+            {isErrorMessageVisible && (
+              <IWillFollowYou errorMessage={errorMessage} />
+            )}
+          </div>
+          <div
+            className="right-section"
+            style={{
+              boxSizing: "border-box",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
+              height: "100%",
+              // overflow: "hidden",
+            }}
+          >
+            <Form.Item
+              name="siteName"
+              label="Site Name"
+              rules={[
+                {
+                  required: true,
+                  message: "Site Name is required",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            {/* Add more form fields as needed */}
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+            &nbsp;
+          </div>
+        </div>
+      </div>
+      <Modal open={previewOpen} footer={null} onCancel={handleCancel}>
+        {previewImage && (
+          <>
+            <img alt="example" style={{ width: "100%" }} src={previewImage} />
+            <div
+              style={{
+                flex: 1,
+                width: "100%",
+                color: "#f3f6f9",
+                borderRadius: 16,
+                padding: "0 0 0 16",
+                top: 0,
+                justifyContent: "center",
+                backgroundColor: "#00a991",
+                fontSize: 12,
+              }}
+            >
+              <p>File Name: {fileInfo.name}</p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div
+                style={{
+                  flex: 1,
+                  color: "#f3f6f9",
+                  borderRadius: 16,
+                  padding: "0 0 0 16",
+                  margin: 4,
+                  height: "20",
+                  top: 0,
+                  justifyContent: "center",
+                  backgroundColor: "#00a991",
+                  fontSize: 12,
+                }}
+              >
+                <p style={{margin:-12px 0px}}>File Size: {formatBytes(fileInfo.size || 0)}</p>
+              </div>
+              <div
+                style={{
+                  flex: 1,
+                  color: "#f3f6f9",
+                  borderRadius: 16,
+                  padding: "0 0 0 16",
+                  top: 0,
+                  margin: 4,
+                  height: "auto",
+                  justifyContent: "center",
+                  backgroundColor: "#00a991",
+                  fontSize: 12,
+                }}
+              >
+                <p>Upload Date: {fileInfo.uploadDate?.toLocaleString()}</p>
+              </div>
+            </div>
+            <Button onClick={() => window.print()}>Print</Button>
+          </>
+        )}
+      </Modal>
+    </Form>
+  );
+};
+
+export default SiteVisitApprovalForm;
---
+++ .history/src/pages/NapsWizard/NapsWizard2_20230730130011.tsx
@@ -0,0 +1,218 @@
+import { PageContainer } from "@ant-design/pro-components";
+import { Button, Steps, message } from "antd";
+import React from "react";
+import BillPaymentApprovalForm from "../Forms/BillPaymentApprovalForm";
+import CompleteMeterInstallationForm from "../Forms/CompleteMeterInstallationForm";
+import ConnectionApprovalForm from "../Forms/ConnectionApprovalForm";
+import GetNeighbourAccountForm from "../Forms/GetNeighbourAccountForm";
+import IssueDepositAndInstallationBillForm from "../Forms/IssueDepositAndInstallationBillForm";
+import PlanApprovalInfoForm from "../Forms/PlanApprovalInfoForm";
+import SiteInspectionApprovalForm from "../Forms/SiteInspectionApprovalForm_";
+import SiteVisitApprovalForm, {
+  SiteVisitApprovalFormProps,
+} from "../Forms/SiteVisitApprovalForm";
+
+import "./napsWizard2.css";
+
+const { Step } = Steps;
+
+interface Theme {
+  [key: string]: string;
+}
+
+// Define a union type for all the form props
+type FormProps = SiteVisitApprovalFormProps;
+
+// Update the forms array with the correct type
+const forms: React.ComponentType<FormProps>[] = [
+  SiteVisitApprovalForm,
+  PlanApprovalInfoForm,
+  SiteInspectionApprovalForm,
+  GetNeighbourAccountForm,
+  BillPaymentApprovalForm,
+  ConnectionApprovalForm,
+  IssueDepositAndInstallationBillForm,
+  CompleteMeterInstallationForm,
+];
+
+const NapsWizard2: React.FC<{ theme: Theme }> = ({ theme }) => {
+  const [currentStep, setCurrentStep] = React.useState(0);
+  const totalSteps = forms.length;
+  const [completedSteps, setCompletedSteps] = React.useState(0);
+  const [pendingSteps, setPendingSteps] = React.useState(totalSteps - 1);
+
+  const handleNext = () => {
+    setCompletedSteps(currentStep + 1);
+    setPendingSteps(totalSteps - (currentStep + 1) - 1);
+    setCurrentStep(currentStep + 1);
+  };
+
+  const handlePrev = () => {
+    if (currentStep > 0) {
+      setCompletedSteps(currentStep - 1);
+      setPendingSteps(totalSteps - (currentStep - 1) - 1);
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  const handleFinish = () => {
+    message.success("All steps completed!");
+  };
+
+  const getStepDisplay = (stepIndex: number) => {
+    if (stepIndex < completedSteps) {
+      return <span className="step">{stepIndex + 1}</span>;
+    } else if (stepIndex === completedSteps) {
+      return <span className="current-step">{stepIndex + 1}</span>;
+    } else if (stepIndex === completedSteps + 1) {
+      return <span className="step-number">{stepIndex + 1}</span>;
+    } else if (stepIndex === completedSteps + 2) {
+      return <span className="step-number">{stepIndex + 1}</span>;
+    } else {
+      return (
+        <span className="step-number">{completedSteps + 3 - stepIndex}</span>
+      );
+    }
+  };
+
+  const renderCircularProgress = () => {
+    const radius = 30; // Adjust the radius as needed
+    const circumference = 2 * Math.PI * radius;
+    const progress = (completedSteps / (totalSteps - 1)) * 100;
+    const offset = circumference - (progress / 100) * circumference;
+
+    return (
+      <div style={{ height: "100%", overflowY: "auto" }}>
+        <div className="circular-progress-container">
+          <svg
+            className="circular-progress"
+            height={radius * 2}
+            width={radius * 2}
+          >
+            <circle
+              className="progress-background"
+              cx={radius}
+              cy={radius}
+              r={radius - 3} // Adjust the thickness of the progress ring
+              fill="transparent"
+              stroke={theme["orange.1"]}
+              strokeWidth="2" // Adjust the thickness of the progress ring
+            />
+            <circle
+              className="progress"
+              cx={radius}
+              cy={radius}
+              r={radius - 3} // Adjust the thickness of the progress ring
+              fill="transparent"
+              stroke={theme["orange.6"]} // Adjust the color as needed
+              strokeWidth="1" // Adjust the thickness of the progress ring
+              strokeDasharray={circumference}
+              strokeDashoffset={offset}
+            />
+          </svg>
+          <div className="current-step-indicator">
+            <div className="step-indicator"></div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const calculateInitialStickyPosition = () => {
+    // Get the maximum height of the screen
+    const maxHeight = window.innerHeight;
+    // Calculate the position for the sticky footer
+    const initialPosition = maxHeight - 100; // Adjust the value as needed
+    return `${initialPosition}px`;
+  };
+
+  return (
+    <div>
+      <PageContainer>
+        <div className="steps-container">
+          <div className="completed-steps">
+            {[...Array(Math.min(completedSteps, 2))].map((_, index) => (
+              <div key={index} className="">
+                {getStepDisplay(index)}
+              </div>
+            ))}
+            {completedSteps > 2 && (
+              <div className="step-number" style={{ marginLeft: 12 }}>
+                +{completedSteps - 2}
+              </div>
+            )}
+          </div>
+          <div className="current-step">
+            <div className="step">
+              {" "}
+              <h2>{currentStep < totalSteps ? ` ${currentStep + 1}` : " "}</h2>
+            </div>
+            {/* Add the circular progress indicator here */}
+            {renderCircularProgress()}
+          </div>
+          <div className="pending-steps">
+            {Array.from({ length: Math.min(pendingSteps, 2) }).map(
+              (_, index) => (
+                <div key={index} className="step">
+                  {getStepDisplay(completedSteps + index + 1)}
+                </div>
+              )
+            )}
+            {pendingSteps > 2 && (
+              <div className="step-number" style={{ marginLeft: 54 }}>
+                +{pendingSteps - 2}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="steps-content">
+          {currentStep >= 0 && currentStep < totalSteps ? (
+            <div>
+              <h2>{`Step ${currentStep + 1}`}</h2>
+              {/* Render the form component based on the current step */}
+              {React.createElement(forms[currentStep])}
+            </div>
+          ) : null}
+        </div>
+        <div
+          style={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            marginLeft: -16,
+            marginRight: -16,
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "12 16px",
+            borderRadius: 8,
+            backgroundColor: `${theme["cyan.3"]}50`, // Apply transparency to the background color
+            backdropFilter: "blur(10px)", // Apply the blur filter
+            zIndex: 999,
+          }}
+        >
+          <div style={{ flex: "1" }}>
+            {currentStep > 0 && <Button onClick={handlePrev}>Previous</Button>}
+          </div>
+
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {currentStep < totalSteps - 1 && (
+              <Button type="primary" onClick={handleNext}>
+                Next
+              </Button>
+            )}
+            {currentStep === totalSteps - 1 && (
+              <Button type="primary" onClick={handleFinish}>
+                Done
+              </Button>
+            )}
+          </div>
+        </div>
+      </PageContainer>
+    </div>
+  );
+};
+
+export default NapsWizard2;
