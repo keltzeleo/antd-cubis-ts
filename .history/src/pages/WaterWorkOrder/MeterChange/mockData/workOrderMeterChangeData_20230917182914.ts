3e0809bf690012d9d18bf16cff9488d6c946eb64
keltzeleo <65336730+keltzeleo@users.noreply.github.com>
2023-09-17 20:05:49 +0800

    added mock data and created custom button spotlight button

---
+++ .history/src/pages/WaterWorkOrder/MeterChange/mockData/workOrderMeterChangeData_20230917182914.ts
@@ -0,0 +1,63 @@
+// mock_data/workOrderData.ts
+
+export const workOrderData = {
+  issueWorkOrder: {
+    accounts: [
+      {
+        accountNo: "ACC123",
+        accountStatus: "Active",
+        tariff: "T1",
+        consumerType: "Residential",
+        name: "John Doe",
+        bookNo: "BK001",
+        accountType: "Standard",
+        arrears: 0,
+        address: "123 Main St, City, State",
+        meterNo: "MTR123",
+        meterStatus: "Active",
+        lastControlReading: "50",
+        lastActualReading: "55",
+        lastReadCode: "R1",
+        replacedMeterConsumption: "5",
+        meterFaulty: false,
+        workOrderType: "Type1",
+        workOrderDate: "2023-09-15",
+        workOrderIssueBy: "Admin",
+        workOrderNo: "WO123",
+        printWorkOrder: false,
+        workOrderStatus: "New",
+        scheduleStartDate: "2023-09-16",
+        scheduleEndDate: "2023-09-17",
+        scheduleStartTime: "09:00",
+        workOrderRemark: "Initial issue",
+        departmentInCharge: "Dept1",
+        assignTo: "Team1",
+        meterRemark: "Working",
+      },
+    ],
+    workOrderTypes: ["Type1", "Type2", "Type3"],
+  },
+  completeWorkOrder: {
+    accounts: [
+      {
+        accountNo: "ACC123",
+        completionDate: "2023-09-17",
+        completionRemark: "Job done",
+        completionStatus: "Completed",
+        // ...other fields from issueWorkOrder can also be here
+      },
+    ],
+    workOrderTypes: ["Type1", "Type2", "Type3"],
+  },
+  cancelWorkOrder: {
+    accounts: [
+      {
+        accountNo: "ACC123",
+        cancelDate: "2023-09-16",
+        cancelReason: "Customer request",
+        // ...other fields from issueWorkOrder can also be here
+      },
+    ],
+    workOrderTypes: ["Type1", "Type2", "Type3"],
+  },
+};
